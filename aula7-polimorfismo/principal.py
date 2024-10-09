768ad4e44c5cabe236e1cf0ad490c927584aa680
Samuka <araujodiassamuel6@gmail.com>
2024-10-09 09:00:30 -0300

    polimorfismo

---
+++ aula7-polimorfismo/principal.py
@@ -0,0 +1,8 @@
+from produto import Produtos,Livro
+
+produto1 = Produtos("Mesa",859.99)
+livro1 = Livro("Mémorias Póstumas", 51.56,"Machado de Asis")
+
+produto1.descrever()
+livro1.descrever()
+livro1.calcularDesconto()
